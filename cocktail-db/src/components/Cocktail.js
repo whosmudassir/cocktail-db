33dfc5b221bb854fc2b796886120406d30dd1fca
Mudassir Khan <deadsoulartist@gmail.com>
2021-05-16 06:34:23 +0530

    update

--- cocktail-db/src/components/Cocktail.js
+++ cocktail-db/src/components/Cocktail.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Cocktail = () => {
-  return <div></div>;
+const Cocktail = ({ image, name, id, info, glass }) => {
+  return (
+    <article className="cocktail">
+      <div calssName="img-container">
+        <img src={image} alt={name} />
+      </div>
+
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn-primary">
+          Details
+        </Link>
+      </div>
+    </article>
+  );
 };
 
 export default Cocktail;
